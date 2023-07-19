8c891331df16d70bbe9b3992f52a2bc9d8940083
“Muhammad-Yasin-Alqurni” <yalqurni@gmail.com>
2023-07-19 16:53:33 +0700

    fix unit test

--- src/student/services/student_service_test.go
+++ src/student/services/student_service_test.go
@@ -1,58 +1,19 @@
 package service_test
 
 import (
-	"context"
-	model "github-dbq/src/student/models"
+	repository "github-dbq/mock/student/repository"
+	request "github-dbq/src/student/http/requests"
 	service "github-dbq/src/student/services"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type mockStudentRepository struct{}
-
-func (m *mockStudentRepository) Get(ctx context.Context) ([]*model.Student, error) {
-
-	student := &model.Student{
-		Id:          1,
-		Name:        "John Doe",
-		Age:         20,
-		DateOfBirth: "2003-01-01",
-		UpdatedAt:   time.Now(),
-		CreatedAt:   time.Now(),
-	}
-	return []*model.Student{student}, nil
-}
-
-func (m *mockStudentRepository) GetByID(id uint, ctx context.Context) (*model.Student, error) {
-
-	student := &model.Student{
-		Id:          1,
-		Name:        "John Doe",
-		Age:         20,
-		DateOfBirth: "2003-01-01",
-		UpdatedAt:   time.Now(),
-		CreatedAt:   time.Now(),
-	}
-	return student, nil
-}
-
-func (m *mockStudentRepository) Update(name string, id uint, ctx context.Context) error {
-	return nil
-}
-
-func (m *mockStudentRepository) Delete(id uint, ctx context.Context) error {
-	return nil
-}
-
-func (m *mockStudentRepository) Create(data model.Student, ctx context.Context) error {
-	return nil
-}
+var repo = repository.MockStudentRepository{}
 
 func TestGet(t *testing.T) {
-	repo := &mockStudentRepository{}
-	service := service.StudentServiceImpl(repo)
+
+	service := service.StudentServiceImpl(&repo)
 
 	students, err := service.Get()
 
@@ -61,8 +22,7 @@ func TestGet(t *testing.T) {
 }
 
 func TestGetByID(t *testing.T) {
-	repo := &mockStudentRepository{}
-	service := service.StudentServiceImpl(repo)
+	service := service.StudentServiceImpl(&repo)
 
 	studentId := 1
 	student, err := service.GetByID(uint(studentId))
@@ -72,8 +32,7 @@ func TestGetByID(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	repo := &mockStudentRepository{}
-	service := service.StudentServiceImpl(repo)
+	service := service.StudentServiceImpl(&repo)
 
 	studentId := 1
 	name := "name"
@@ -82,24 +41,23 @@ func TestUpdate(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-// func TestDelete(t *testing.T) {
-// 	repo := &mockStudentRepository{}
-// 	service := StudentServiceImpl(repo)
+func TestDelete(t *testing.T) {
+	service := service.StudentServiceImpl(&repo)
 
-// 	studentID := uint(1)
-// 	err := service.Delete(studentID)
-// 	assert.NoError(t, err)
-// }
+	studentID := uint(1)
+	err := service.Delete(studentID)
+	assert.NoError(t, err)
+}
 
-// func TestCreate(t *testing.T) {
-// 	repo := &mockStudentRepository{}
-// 	service := StudentServiceImpl(repo)
+func TestCreate(t *testing.T) {
 
-// 	studentRequest := requests.CreateRequest{
-// 		Name:        "Jane Doe",
-// 		Age:         21,
-// 		DateOfBirth: "2002-01-01",
-// 	}
-// 	err := service.Create(studentRequest)
-// 	assert.NoError(t, err)
-// }
+	service := service.StudentServiceImpl(&repo)
+
+	studentRequest := request.CreateRequest{
+		Name:        "yasin",
+		Age:         12,
+		DateOfBirth: "12-12-1995",
+	}
+	err := service.Create(studentRequest)
+	assert.NoError(t, err)
+}
